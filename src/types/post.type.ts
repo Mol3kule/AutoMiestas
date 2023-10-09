1d8dfb590ffa83c294ffb8a2d0152c0ced71e5f4
Molekule <49242127+Mol3kule@users.noreply.github.com>
2023-10-09 13:41:08 +0300

    INIT

---
+++ src/types/post.type.ts
@@ -0,0 +1,18 @@
+export type PostInformation = {
+    title: string;
+    description: string;
+}
+
+export type PostImage = {
+    url: string;
+}
+
+export type PostPeriods = {
+    time_created: number;
+    time_due: number;
+}
+
+export type PostBoosts = {
+    time_created: number;
+    time_due: number;
+}
